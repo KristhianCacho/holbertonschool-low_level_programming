4fa20a5e23e2a879741a7802d81fc18145872ff6
KristhianCacho <kristhiancacho518@gmail.com>
2023-08-09 17:31:08 -0700

    0 Putchar

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,5 +1,8 @@
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
 void print_alphabet(void);
+int _putchar(char c);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -10,4 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+
+#endif
