e0fac94a35f5b0899e624137fd9891bed86712d5
KristhianCacho <kristhiancacho518@gmail.com>
2023-08-22 08:42:05 -0700

    0 Whatsmyname

---
+++ argc_argv/main.h
@@ -0,0 +1,11 @@
+int _putchar(char c);
+
+int main(int argc, char *argv[]);
+
+int main(int argc, char *argv[]__attribute((unused)));
+
+int main(int argc, char *argv[]);
+
+int main(int argc, char *argv[]);
+
+int main(int argc, char *argv[]);
